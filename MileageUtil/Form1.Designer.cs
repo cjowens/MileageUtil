811827607f59d75ff02136c96e2aa95dac3ef804
Chris Owens <chris.owens@desktopdaydreams.com>
2015-01-07 17:48:14 +0000

    - Added spreadsheet creation/saving/loading - Moved UI around

--- MileageUtil/Form1.Designer.cs
+++ MileageUtil/Form1.Designer.cs
@@ -33,7 +33,7 @@
             this.btnAddPostcode = new System.Windows.Forms.Button();
             this.Calendar = new System.Windows.Forms.MonthCalendar();
             this.btnOpenSpreadsheet = new System.Windows.Forms.Button();
-            this.FileLabel = new System.Windows.Forms.Label();
+            this.fileLabel = new System.Windows.Forms.Label();
             this.btnInsertData = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnCreateSpreadsheet = new System.Windows.Forms.Button();
@@ -41,26 +41,30 @@
             this.createSpreadsheetDialogue = new System.Windows.Forms.SaveFileDialog();
             this.btnRemoveLastPostcode = new System.Windows.Forms.Button();
             this.btnClearAll = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnShowMileage = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // postcodes
             // 
             this.postcodes.FormattingEnabled = true;
-            this.postcodes.Location = new System.Drawing.Point(13, 180);
+            this.postcodes.Location = new System.Drawing.Point(254, 87);
             this.postcodes.Name = "postcodes";
-            this.postcodes.Size = new System.Drawing.Size(113, 108);
+            this.postcodes.Size = new System.Drawing.Size(113, 134);
             this.postcodes.TabIndex = 0;
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(132, 180);
+            this.txtInput.Location = new System.Drawing.Point(376, 75);
             this.txtInput.Name = "txtInput";
             this.txtInput.Size = new System.Drawing.Size(108, 20);
             this.txtInput.TabIndex = 1;
             // 
             // btnAddPostcode
             // 
-            this.btnAddPostcode.Location = new System.Drawing.Point(132, 206);
+            this.btnAddPostcode.Location = new System.Drawing.Point(376, 101);
             this.btnAddPostcode.Name = "btnAddPostcode";
             this.btnAddPostcode.Size = new System.Drawing.Size(108, 23);
             this.btnAddPostcode.TabIndex = 2;
@@ -70,13 +74,13 @@
             // 
             // Calendar
             // 
-            this.Calendar.Location = new System.Drawing.Point(13, 6);
+            this.Calendar.Location = new System.Drawing.Point(15, 59);
             this.Calendar.Name = "Calendar";
             this.Calendar.TabIndex = 3;
             // 
             // btnOpenSpreadsheet
             // 
-            this.btnOpenSpreadsheet.Location = new System.Drawing.Point(132, 294);
+            this.btnOpenSpreadsheet.Location = new System.Drawing.Point(144, 293);
             this.btnOpenSpreadsheet.Name = "btnOpenSpreadsheet";
             this.btnOpenSpreadsheet.Size = new System.Drawing.Size(108, 23);
             this.btnOpenSpreadsheet.TabIndex = 4;
@@ -84,26 +88,29 @@
             this.btnOpenSpreadsheet.UseVisualStyleBackColor = true;
             this.btnOpenSpreadsheet.Click += new System.EventHandler(this.OpenSpreadsheetDialogue);
             // 
-            // FileLabel
+            // fileLabel
             // 
-            this.FileLabel.AutoSize = true;
-            this.FileLabel.Location = new System.Drawing.Point(357, 22);
-            this.FileLabel.Name = "FileLabel";
-            this.FileLabel.Size = new System.Drawing.Size(0, 13);
-            this.FileLabel.TabIndex = 5;
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Location = new System.Drawing.Point(12, 325);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(29, 13);
+            this.fileLabel.TabIndex = 5;
+            this.fileLabel.Text = "label";
+            this.fileLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // btnInsertData
             // 
-            this.btnInsertData.Location = new System.Drawing.Point(13, 323);
+            this.btnInsertData.Location = new System.Drawing.Point(13, 341);
             this.btnInsertData.Name = "btnInsertData";
             this.btnInsertData.Size = new System.Drawing.Size(227, 56);
             this.btnInsertData.TabIndex = 6;
             this.btnInsertData.Text = "Insert Data To Spreadsheet";
             this.btnInsertData.UseVisualStyleBackColor = true;
+            this.btnInsertData.Click += new System.EventHandler(this.InsetDataToSpreadsheet);
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(13, 385);
+            this.btnClose.Location = new System.Drawing.Point(12, 432);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(227, 23);
             this.btnClose.TabIndex = 7;
@@ -123,8 +130,7 @@
             // 
             // openSpreadsheetDialogue
             // 
-            this.openSpreadsheetDialogue.FileName = "spreadsheet";
-            this.openSpreadsheetDialogue.Filter = "Excel Files (*xls, *.xlsx)|*.xls;*.xlsx";
+            this.openSpreadsheetDialogue.Filter = "Excel Files (*xls)|*.xls;";
             // 
             // createSpreadsheetDialogue
             // 
@@ -133,7 +139,7 @@
             // 
             // btnRemoveLastPostcode
             // 
-            this.btnRemoveLastPostcode.Location = new System.Drawing.Point(132, 235);
+            this.btnRemoveLastPostcode.Location = new System.Drawing.Point(376, 130);
             this.btnRemoveLastPostcode.Name = "btnRemoveLastPostcode";
             this.btnRemoveLastPostcode.Size = new System.Drawing.Size(108, 23);
             this.btnRemoveLastPostcode.TabIndex = 9;
@@ -143,7 +149,7 @@
             // 
             // btnClearAll
             // 
-            this.btnClearAll.Location = new System.Drawing.Point(132, 264);
+            this.btnClearAll.Location = new System.Drawing.Point(376, 159);
             this.btnClearAll.Name = "btnClearAll";
             this.btnClearAll.Size = new System.Drawing.Size(108, 23);
             this.btnClearAll.TabIndex = 10;
@@ -151,22 +157,65 @@
             this.btnClearAll.UseVisualStyleBackColor = true;
             this.btnClearAll.Click += new System.EventHandler(this.ClearAllPostcodes);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 403);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(226, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(388, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Enter postcode:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(269, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Waypoints";
+            // 
+            // btnShowMileage
+            // 
+            this.btnShowMileage.Location = new System.Drawing.Point(376, 188);
+            this.btnShowMileage.Name = "btnShowMileage";
+            this.btnShowMileage.Size = new System.Drawing.Size(108, 33);
+            this.btnShowMileage.TabIndex = 14;
+            this.btnShowMileage.Text = "Show Mileage";
+            this.btnShowMileage.UseVisualStyleBackColor = true;
+            this.btnShowMileage.Click += new System.EventHandler(this.ShowMileage);
+            // 
             // EntryForm
             // 
             this.AcceptButton = this.btnAddPostcode;
-            this.ClientSize = new System.Drawing.Size(253, 416);
+            this.ClientSize = new System.Drawing.Size(514, 491);
+            this.Controls.Add(this.btnShowMileage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnClearAll);
             this.Controls.Add(this.btnRemoveLastPostcode);
             this.Controls.Add(this.btnCreateSpreadsheet);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnInsertData);
-            this.Controls.Add(this.FileLabel);
+            this.Controls.Add(this.fileLabel);
             this.Controls.Add(this.btnOpenSpreadsheet);
             this.Controls.Add(this.Calendar);
             this.Controls.Add(this.btnAddPostcode);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.postcodes);
+            this.DoubleBuffered = true;
             this.Name = "EntryForm";
+            this.Text = "Mileage Calculation Tool";
+            this.Load += new System.EventHandler(this.OnFormLoaded);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,7 +228,7 @@
         private System.Windows.Forms.Button btnAddPostcode;
         private System.Windows.Forms.MonthCalendar Calendar;
         private System.Windows.Forms.Button btnOpenSpreadsheet;
-        private System.Windows.Forms.Label FileLabel;
+        private System.Windows.Forms.Label fileLabel;
         private System.Windows.Forms.Button btnInsertData;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnCreateSpreadsheet;
@@ -187,6 +236,10 @@
         private System.Windows.Forms.SaveFileDialog createSpreadsheetDialogue;
         private System.Windows.Forms.Button btnRemoveLastPostcode;
         private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnShowMileage;
     }
 }
 
--- MileageUtil/Form1.cs
+++ MileageUtil/Form1.cs
@@ -1,21 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.IO;
-using System.Threading.Tasks;
+using System.Net;
+using System.Xml;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using ExcelLibrary.SpreadSheet;
+
 
 namespace MileageUtil
 {
     public partial class EntryForm : Form
     {
+        private Workbook _workBook = null;                                  // current spreadsheet
+        private string _currentFilePath = "";                               // file path of the currently loaded spreadsheed
+        private List<string> _postcodeWaypoints = new List<string>();       // seperate postcode list for manipulation
+        private const string _pathLabel = "Current Spreadsheet:\n";         // currently loaded spreadsheet label message
+
+        // comprehensive UK postcode regex supplied by UK Gov.
+        private const string _postcodeRegex = "(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9][0-9]?)|(([A-Z-[QVX]][0-9][A-HJKSTUW])|([A-Z-[QVX]][A-Z-[IJZ]][0-9][ABEHMNPRVWXY])))) ?[0-9][A-Z-[CIKMOV]]{2})";
+
+
         public EntryForm ()
         {
             InitializeComponent();
+        }
+
+        // Initialization. Called before application is rendered for 1st time.
+        private void OnFormLoaded (object sender, EventArgs e)
+        {
+            _workBook = new Workbook();
+            fileLabel.Text = _pathLabel;
         }
 
         private void AddPostCode (object sender, EventArgs e)
@@ -23,51 +39,126 @@ namespace MileageUtil
             // if the text box isn't empty
             if (!string.IsNullOrEmpty(txtInput.Text))
             {
-                // add the entry to the listbox and clear the text field
-                postcodes.Items.Add(txtInput.Text);
-                txtInput.Text = "";
+                var match = Regex.Match(txtInput.Text, _postcodeRegex);
+
+                if (match.Success)
+                {
+                    // add the entry to the listbox
+                    postcodes.Items.Add(txtInput.Text);
+                    // reset the text field
+                    txtInput.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Please use a valid postcode.\nUppercase required.");
+                }
             }
         }
 
         private void CreateSpreadsheetDialogue (object sender, EventArgs e)
         {
+            createSpreadsheetDialogue.FileName = "";
             createSpreadsheetDialogue.ShowDialog();
         }
 
         private void OpenSpreadsheetDialogue (object sender, EventArgs e)
         {
+            openSpreadsheetDialogue.FileName = "";
             var result = openSpreadsheetDialogue.ShowDialog();
             if (result == DialogResult.OK)
             {
                 string file = openSpreadsheetDialogue.FileName;
                 try
                 {
-                    FileLabel.Text = file;
+                    _workBook = Workbook.Load(file);
+                    _currentFilePath = openSpreadsheetDialogue.FileName;
                 }
-                catch(IOException)
+                catch(IOException ioExcep)
                 {
+                    MessageBox.Show("ERROR: " + ioExcep.ToString());
+                    return;
                 }
+
+                fileLabel.Text = _pathLabel + _currentFilePath;
             }
         }
 
-        private void CreateSpreadsheet(object sender, CancelEventArgs e)
+        private void InsetDataToSpreadsheet (object sender, EventArgs e)
         {
-            MessageBox.Show("Spreadsheet created.");
+            if (postcodes.Items.Count == 0)
+            {
+                MessageBox.Show("There nothing to enter in to spreadsheet.\nPlease enter your trip and try again.");
+                return;
+            }
         }
 
+        private void CreateSpreadsheet (object sender, CancelEventArgs e)
+        {
+            _currentFilePath = createSpreadsheetDialogue.FileName;
+            fileLabel.Text = _pathLabel + _currentFilePath;
+            _workBook.Save(_currentFilePath);
+        }
+
+        // Removes the last postcode from the list
+        private void RemoveLastPostcode (object sender, EventArgs e)
+        {
+            if (postcodes.Items.Count > 0)
+                postcodes.Items.RemoveAt(postcodes.Items.Count - 1);
+        }
+
+        // Removes all postcodes from the list
+        private void ClearAllPostcodes (object sender, EventArgs e)
+        {
+            if (postcodes.Items.Count > 0)
+                postcodes.Items.Clear();
+        }
+
+        // Closes the application
         private void CloseApplication (object sender, EventArgs e)
         {
+            GC.Collect();
             Application.Exit();
         }
 
-        private void RemoveLastPostcode (object sender, EventArgs e)
+        private void ShowMileage (object sender, EventArgs e)
         {
+            if(postcodes.Items.Count == 0)
+            {
+                MessageBox.Show("There was nothing to check the milage of.\nPlease enter your trip and try again.");
+                return;
+            }
+
 
         }
 
-        private void ClearAllPostcodes (object sender, EventArgs e)
+        public static double GetTotalMileage()
         {
+            return 0;
+        }
 
+        public static double GetDrivingDistanceInMiles (string origin, string destination)
+        {
+            string url = @"http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" +
+              origin + "&destinations=" + destination +
+              "&mode=driving&sensor=false&language=en-EN&units=imperial&alternatives=true";
+
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader sreader = new StreamReader(dataStream);
+            string responsereader = sreader.ReadToEnd();
+            response.Close();
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(responsereader);
+
+            if (xmldoc.GetElementsByTagName("status")[0].ChildNodes[0].InnerText == "OK")
+            {
+                XmlNodeList distance = xmldoc.GetElementsByTagName("distance");
+                return Convert.ToDouble(distance[0].ChildNodes[1].InnerText.Replace(" mi", ""));
+            }
+
+            return 0;
         }
     }
 }
